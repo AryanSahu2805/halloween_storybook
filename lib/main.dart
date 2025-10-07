f36e82c58b3d40ee5ccc7196e2d0274100ab5ddd
Kanzanbu <166258575+Kanzanbu@users.noreply.github.com>
2025-10-07 18:50:04 -0400

    Update main.dart
    audio package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'screens/home_screen.dart';
 import 'screens/game_screen.dart';
 import 'screens/win_screen.dart';
@@ -8,8 +9,37 @@ void main() {
   runApp(const HalloweenGameApp());
 }
 
-class HalloweenGameApp extends StatelessWidget {
+class HalloweenGameApp extends StatefulWidget {
   const HalloweenGameApp({Key? key}) : super(key: key);
+
+  @override
+  State<HalloweenGameApp> createState() => _HalloweenGameAppState();
+}
+
+class _HalloweenGameAppState extends State<HalloweenGameApp> {
+  late final AudioPlayer _audioPlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    _initMusic();
+  }
+
+  Future<void> _initMusic() async {
+    _audioPlayer = AudioPlayer();
+    // Set looping and volume
+    await _audioPlayer.setReleaseMode(ReleaseMode.loop);
+    await _audioPlayer.setVolume(0.3);
+
+    // Play the spooky music from assets
+    await _audioPlayer.play(AssetSource('sounds/spookymusic.mp3'));
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +51,6 @@ class HalloweenGameApp extends StatelessWidget {
         brightness: Brightness.dark,
         scaffoldBackgroundColor: AppColors.darkBackground,
       ),
-      // Named routes for navigation
       initialRoute: '/',
       routes: {
         '/': (context) => const HomeScreen(),
